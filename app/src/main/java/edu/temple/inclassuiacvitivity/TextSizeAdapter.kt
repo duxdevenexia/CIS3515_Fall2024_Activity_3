b53c1b1b0ef970ee7999d29012765acfdddc2963
duxdevenexia <88348139+duxdevenexia@users.noreply.github.com>
2024-09-19 12:16:25 -0400

    update

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -1,25 +1,43 @@
 package edu.temple.inclassuiacvitivity
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
 
-class TextSizeAdapter : BaseAdapter() {
+
+
+class TextSizeAdapter(private val context: Context, private val numbers: Array<Int>) : BaseAdapter() {
+
+    /*
+    // initial ary
+    private val ary: List<Int> = mutableListOf()
+     */
 
     override fun getCount(): Int {
-        TODO("Not yet implemented")
+
+        return numbers.size
     }
 
     override fun getItem(position: Int): Any {
-        TODO("Not yet implemented")
+        return numbers[position]
     }
 
-    override fun getItemId(position: Int): Long {
-        TODO("Not yet implemented")
-    }
+    override fun getItemId(position: Int): Long = numbers[position].toLong()
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         TODO("Not yet implemented")
+        val view: View = convertView ?: LayoutInflater.from(parent?.context)
+            .inflate(android.R.layout.simple_spinner_item, parent, false)
+
+        val item = getItem(position)
+
+        val textView = view
+
+
+        return view
     }
 
 }
